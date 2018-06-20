6c5fec55916c89b3bec5313f1510cfa2a84bb52d
Johny M <torac0011@users.noreply.github.com>
2018-06-20 23:02:09 +0200

    Update dynamics_numeric.m

--- dynamics_numeric.m
+++ dynamics_numeric.m
@@ -12,14 +12,16 @@ classdef dynamics_numeric < handle
         EMF
         MPC
         control_torque = [0; 0; 0]
+        control
+        control_model
     end
     
     methods
-        function obj = dynamics_numeric(delta_t, inertia, Q, R)
+        function obj = dynamics_numeric(delta_t, inertia, Q, R, control_model)
             % state vector x = [q1 q2 q3 q4 w1 w2 w3]'
             dynamics_symbolic_derivation
-            f__ = matlabFunction(subs(f, [dt; I11; I22; I33], [delta_t; inertia']), 'Vars', {x, M, B});
-            obj.f = @(x_, u, B) f__(x_(1), x_(2), x_(3), x_(4), x_(5), x_(6), x_(7), u(1), u(2), u(3), B(1), B(2), B(3));
+            obj.f = matlabFunction(subs(f, [dt; I11; I22; I33], [delta_t; inertia']), 'Vars', {x, M, B});
+            %obj.f = @(x_, u, B) f__(x_(1), x_(2), x_(3), x_(4), x_(5), x_(6), x_(7), u(1), u(2), u(3), B(1), B(2), B(3));
             F__ = matlabFunction(subs(F, [dt; I11; I22; I33], [delta_t; inertia']), 'Vars', x);
             obj.Phi = @(x_, u) F__(x_(1), x_(2), x_(3), x_(4), x_(5), x_(6), x_(7));
             % input vector u = [M1 M2 M3]'
@@ -34,24 +36,36 @@ classdef dynamics_numeric < handle
             obj.R = R;
             obj.Kalman = extended_kalman_filter(7);
             obj.delta_t = delta_t;
+            obj.control_model = control_model;
             
-            %TODO here goes the object definition of geomagnetic field reference DONE
-            obj.EMF = b_dot(delta_t, inertia, 500000); % 3rd arg - altitude
-            %TODO here goes obj defintion of model_predictive DONE
-            % covariance matrices for LQR gain
-            Q = [eye(4)*.5 zeros(4,3); zeros(3,4) eye(3)]; %for EKF
-            R = eye(3)*1.6;
-            obj.MPC = model_predictive(delta_t, .5, .5, Q, R);           
+            %TODO if statement parsing control model
+            if (strcmp(control_model, 'LQR'))
+                % weighting matrices for LQR gain
+                Q = diag([1,1,1,1,.1e4,.1e4,.1e4]) %for EKF
+                R = eye(3)*5000%390625%*1e14%diag([0.39,0.42,0.44])*1e-3 %eye(3)
+                obj.control = model_predictive(delta_t, .0016, .0016, Q, R, 'MPC');  
+                obj.EMF = b_dot(delta_t, inertia, 500000); % 3rd arg - altitude
+            elseif strcmp(control_model, 'BDOT')
+                obj.control = b_dot(delta_t, inertia, 500000);
+            else
+                error('invalid control model')
+            end         
             
         end
         
         function update_matrices(self, attitude, rate) % function for body_simulation update
-            F = self.Phi(self.Kalman.x);
-            G = self.G(self.get_mag_moment, self.EMF.get_mf);
-            H = self.H(self.Kalman.x);         
-            x_ref = zeros(1, 6)';
-            self.MPC.update_mpc(F, G, H, x_ref, attitude, rate);
-            self.control_torque = cross(self.get_mag_moment, self.EMF.get_mf);
+%             H = self.H(self.Kalman.x);
+            if strcmp(self.control_model, 'BDOT')
+                self.control.get_bdot_params(rate);
+                self.control_torque = cross(self.get_mag_moment, self.control.get_mf);
+            elseif strcmp(self.control_model, 'LQR')
+                self.EMF.get_bdot_params(rate);
+                F = self.Phi(self.Kalman.x);
+                G = self.G(self.get_mag_moment, self.EMF.get_mf);
+                x_ref = [1; zeros(1, 6)'];
+                self.control.update_mpc(F, G, eye(7), x_ref, attitude, rate);
+                self.control_torque = cross(self.get_mag_moment, self.EMF.get_mf);
+            end
         end
         
         function control_torque = get_torque(self)
@@ -59,7 +73,7 @@ classdef dynamics_numeric < handle
         end
         
         function moment = get_mag_moment(self)
-            moment = self.MPC.get_action_control;
+            moment = self.control.get_action_control;
         end
         
         function renorm_quat(self)
@@ -68,12 +82,14 @@ classdef dynamics_numeric < handle
         end
         
         function predict(self, unused_gyro)
-            self.Kalman.predict(self.f, self.Phi(self.Kalman.x), self.Q)%, self.get_mag_moment, self.EMF.get_mf)
+            x = self.f(self.Kalman.x, self.get_mag_moment, self.EMF.get_mf); %change EMF to control when BDOT
+            self.Kalman.predict(x, self.Phi(self.Kalman.x), self.Q)%, self.get_mag_moment, self.EMF.get_mf)
             self.renorm_quat
         end
         
         function correct(self, Z1, Z2)
-            self.Kalman.correct([Z1; Z2], self.R, self.h, self.H(self.Kalman.x))
+%             self.H
+            self.Kalman.correct([Z1; Z2], self.R, self.h, self.H(self.Kalman.x)) %(self.Kalman.x)
             self.renorm_quat
         end
         
